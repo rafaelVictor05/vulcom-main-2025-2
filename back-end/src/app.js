52aac9f6cd565a9c4d3a2a260828bb695a5acb8c
rafaelVictor05 <rafaelredoval8@gmail.com>
2025-10-11 10:53:24 -0300

    (11/10) Início da transferência do armazenamento do token JWT para cookie

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -10,7 +10,7 @@ const app = express()
 import cors from 'cors'
 app.use(cors({
     origin: process.env.ALLOWED_ORIGINS.split(','),
-    // credentials: true
+    credentials: true
 }))
 
 app.use(logger('dev'))
