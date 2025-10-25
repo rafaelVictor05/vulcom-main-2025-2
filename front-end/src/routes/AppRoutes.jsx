0850f5312a4b41d5b19b3fdabeef8cd8d4231eb4
rafaelVictor05 <rafaelredoval8@gmail.com>
2025-10-25 10:48:10 -0300

    (25/10) Finalização da reorganização das rotas do front-end

--- front-end/src/routes/AppRoutes.jsx
+++ front-end/src/routes/AppRoutes.jsx
@@ -1,51 +1,32 @@
-import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
-import Homepage from '../pages/Homepage'
-
-import CarForm from '../pages/car/CarForm'
-import CarList from '../pages/car/CarList'
-
-import CustomerForm from '../pages/customer/CustomerForm'
-import CustomerList from '../pages/customer/CustomerList'
-
-import UserList from '../pages/user/UserList'
-import UserForm from '../pages/user/UserForm'
-
-import Login from '../pages/Login'
 
 import AuthGuard from './AuthGuard'
 
+
+import { routes, UserLevel } from './routes'
+
+
 export default function AppRoutes() {
-  return <Routes>
-    <Route path="/" element={ <Homepage /> } />
-
-    <Route path="/login" element={ <Login /> } />
-
-    <Route path="/cars" element={ <AuthGuard> <CarList /> </AuthGuard> } />
-    <Route path="/cars/new" element={ <AuthGuard> <CarForm /> </AuthGuard> } />
-    <Route path="/cars/:id" element={ <AuthGuard> <CarForm /> </AuthGuard> } />
-
-    <Route path="/customers" element={ 
-      <AuthGuard> <CustomerList /> </AuthGuard> 
-    } />
-
-    <Route path="/customers/new" element={ 
-      <AuthGuard> <CustomerForm /> </AuthGuard>
-    } />
-    <Route path="/customers/:id" element={ 
-      <AuthGuard> <CustomerForm /> </AuthGuard>  
-    } />
-
-    <Route path="/users" element={ 
-      <AuthGuard adminOnly={true}> <UserList /> </AuthGuard> } 
-    />
-    <Route path="/users/new" element={ 
-      <AuthGuard adminOnly={true}> <UserForm /> </AuthGuard> } 
-    />
-    <Route path="/users/:id" element={ 
-      <AuthGuard adminOnly={true}> <UserForm /> </AuthGuard> } 
-    />
-    
-  </Routes>
+ return (
+   <Routes>
+     {
+       routes.map(route => {
+         let element
+         if(route.userLevel > UserLevel.ANY) {
+           element = <AuthGuard userLevel={route.userLevel}>
+             {route.element}
+           </AuthGuard>
+         }
+         else element = route.element
+        
+         return <Route
+           key={route.route}
+           path={route.route}
+           element={element}
+         />
+       })
+     }
+   </Routes>
+ )
 }
